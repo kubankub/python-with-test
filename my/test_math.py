3d4399d04eabd36cb30e02367187174e5a6f8c90
kubankub <jenyakubik@Galits-MacBook-Air.local>
2018-08-02 19:42:30 +0300

    change math

--- my/test_math.py
+++ my/test_math.py
@@ -1,13 +1,13 @@
 import unittest
-import math
+import my.math
 
 class TestMy(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(math.add(2, 3), 5)
+        self.assertEqual(my.math.add(2, 3), 5)
 
     def test_add2(self):
-        self.assertEqual(math.add(0, -1), -1)
+        self.assertEqual(my.math.add(0, -1), -1)
 
 #    def test_sum(self):
-#        self.assertEqual(math.sum(2, 3, 4), 6)
+#        self.assertEqual(my.math.sum(2, 3, 4), 6)
 
