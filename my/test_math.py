691f0d314c5bb171805f2fe00b4679ae2d9759a1
kubankub <jenyakubik@Galits-MacBook-Air.local>
2018-08-02 19:27:42 +0300

    enable travis

---
+++ my/test_math.py
@@ -0,0 +1,13 @@
+import unittest
+import math
+
+class TestMy(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(math.add(2, 3), 5)
+
+    def test_add2(self):
+        self.assertEqual(math.add(0, -1), -1)
+
+    def test_sum(self):
+        self.assertEqual(math.sum(2, 3, 4), 6)
+
